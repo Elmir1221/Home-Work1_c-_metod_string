4bbf3d7b357e1dd8db5a6392a39d32be1691140d
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-19 14:09:05 +0400

    Add files via upload

---
+++ home work1 _c#2/home work1 _c#2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin.
+// (Cavid gelirse , ekranda divaC gorsensin).
+
+
+
+
+string originalString = "Cavid";
+string reversedString = ReverseString(originalString);
+Console.WriteLine(reversedString);
+   
+string ReverseString(string input)
+{ 
+ char[] charArray = input.ToCharArray();
+ Array.Reverse(charArray);
+ return new string(charArray);
+}
+
+Console.WriteLine();
