f07034243774e55f8f8ac7bc40d58e49ab350e6b
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-20 08:57:52 +0400

    Add files via upload

---
+++ home work/home work1 _c#2/home work1 _c#2/Program.cs
@@ -0,0 +1,94 @@
+﻿// Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin.
+// (Cavid gelirse , ekranda divaC gorsensin).
+
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("MR.Green");
+Console.WriteLine("salam, yazi, reqemi daxil edin ");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("MR.Blue");
+string originalString = Console.ReadLine();
+string reversedString = ReverseString(originalString);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("terse cevrildi");
+Console.WriteLine(reversedString);
+Console.ForegroundColor = ConsoleColor.White;
+
+
+string ReverseString(string input)
+{
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+
+
+#region tekrar
+
+//string originalString = "Cavid ";
+//string reversedString = ReverseString(originalString);
+//Console.WriteLine(reversedString);
+
+//string ReverseString(string input)
+//{ 
+// char[] charArray = input.ToCharArray();
+// Array.Reverse(charArray);
+// return new string(charArray);
+//}
+
+
+//string originalString = "Cavid ";
+//string reversedString = ReverseString(originalString);
+//Console.WriteLine(reversedString);
+
+//string ReverseString(string input)
+//{ 
+// char[] charArray = input.ToCharArray();
+// Array.Reverse(charArray);
+// return new string(charArray);
+//}
+
+
+//string originalString = "Cavid ";
+//string reversedString = ReverseString(originalString);
+//Console.WriteLine(reversedString);
+
+//string ReverseString(string input)
+//{ 
+// char[] charArray = input.ToCharArray();
+// Array.Reverse(charArray);
+// return new string(charArray);
+//}
+
+//string originalString = "Cavid ";
+//string reversedString = ReverseString(originalString);
+//Console.WriteLine(reversedString);
+
+//string ReverseString(string input)
+//{ 
+// char[] charArray = input.ToCharArray();
+// Array.Reverse(charArray);
+// return new string(charArray);
+//}
+
+//string originalString = "Cavid ";
+//string reversedString = ReverseString(originalString);
+//Console.WriteLine(reversedString);
+
+//string ReverseString(string input)
+//{ 
+// char[] charArray = input.ToCharArray();
+// Array.Reverse(charArray);
+// return new string(charArray);
+//}
+#endregion
+
+//int[] arr = { 1, 2, 3, 4 };
+
+//arr[0] = 5;
+//Console.WriteLine(arr[0]);
+
